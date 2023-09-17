c291432b55dbe5c6730c50d5fbbddcde643a42ca
alanyagger <huangxiongfei666@outlook.com>
2023-09-17 17:01:12 +0800

    finished all the c task

--- c-basic/ex10_ll_cycle.c
+++ c-basic/ex10_ll_cycle.c
@@ -3,4 +3,26 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
-}
+    node* fast_ptr=head;
+    node* slow_ptr=head;
+    node* temp_ptr=head;
+    if (head==NULL)
+    {
+    	return 0;
+	}
+    do
+    {
+    	temp_ptr=(*fast_ptr).next;
+    	if (temp_ptr==NULL)
+    	{
+    		return 0;
+		}
+		fast_ptr=(*temp_ptr).next;
+		if (fast_ptr==NULL)
+    	{
+    		return 0;
+		}
+		slow_ptr=(*slow_ptr).next;
+	}while(fast_ptr!=slow_ptr);
+	return 1;
+} 
