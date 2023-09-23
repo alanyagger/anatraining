b0e7785d97888c35aec0d581d424f7c45aba8e0e
alanyagger <huangxiongfei666@outlook.com>
2023-09-23 22:12:01 +0800

    task2

--- upgrade-service/main.cpp
+++ upgrade-service/main.cpp
@@ -2,10 +2,41 @@
 
 using namespace std;
 
-// Feel free to add any global variable, define, typedef, or anything you like
+// Uncomment the following line if you need it
+// typedef long long ll;
 
 int main() {
-	// YOUR CODE HERE
+	long long n=0, m=0, k=0,temp=0,sig=0,temp2=0,i=0;
+	cin.sync_with_stdio(false);
+	cin >> n >> m >> k;
+	long long *arr=new long long[n+1];
+	for (i = 1; i <= n; i++) {
+		long long ai;
+		cin >> ai;
+		arr[i]=ai;
+	}
+    for (i=1;i<=m;i++)
+    {
+    	temp+=arr[i];
+	}
+	sig=1;
+	for(i=2;i<=n-m+1;)
+	{
+		temp2=temp+arr[i+m-1]-arr[i-1];
+		if (temp2<temp)
+		{
+			temp=temp2;
+			sig=i;
+		}
+		i++;
+	}
+	if (temp<(m*k))
+	{
+		cout << sig <<' '<<sig+m-1 << endl;
+	}
+	else{
+		cout << 0<<' '<< 0 << endl;
+	}
 
 	return 0;
 }
