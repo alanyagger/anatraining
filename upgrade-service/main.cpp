4cc8ff85c7845c7ddabcf59baeab06e2147a3158
alanyagger <huangxiongfei666@outlook.com>
2023-09-24 11:33:36 +0800

    222

--- upgrade-service/main.cpp
+++ upgrade-service/main.cpp
@@ -6,42 +6,39 @@ using namespace std;
 // typedef long long ll;
 
 int main() {
-	unsigned long long n, m, k,temp,sig,dir,sum=0,minsum=0;
+	long long n, m, k,temp,sig,did=0,minsum=0;
 	cin.sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	queue<unsigned long long> q1;
-	queue<unsigned long long> q2;
+	queue<long long> q1;
+	queue<long long> q2;
 	cin >> n >> m >> k;
-	for (unsigned long long i = 1; i <= n; i++) {
-		unsigned long long ai;
+	for (long long i = 1; i <= n; i++) {
+		 long long ai;
 		cin >> ai;
 		q1.push(ai);    
 	}
-	for (unsigned long long j=1;j<=m;j++)
+	for (long long j=1;j<=m;j++)
 	{
-		dir=q1.front();
-		sum+=dir;
+		minsum+=q1.front();
+		q2.push(q1.front());
 		q1.pop();
-		q2.push(dir);
 	}
 	temp=1;
 	sig=1;
-	minsum=sum;
 	while(!q1.empty())
 	{
 		temp++;
-		sum=sum+q1.front()-q2.front();
+		did=did+q1.front()-q2.front();
 		q2.push(q1.front());
 		q1.pop();
 		q2.pop();
-		if (sum<minsum)
+		if (did<0)
 		{
-			minsum=sum;
 			sig=temp;
+			minsum+=did;
+			did=0;
 		}
 	}
-	if (minsum < m*k)
+	if ( minsum< m*k)
 	{
 		cout << sig<<' '<<sig+m-1<<endl;
 	}
